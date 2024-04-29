70c5ce6c332bbed32b0515b14205cebc15be4833
Nomad-Rob <rfarley89@gmail.com>
2024-04-29 17:50:31 -0500

    Task 2

---
+++ Webpack/task_2/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+module.exports = {
+  entry: './js/dashboard_main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65
+              },
+              // Can add more later if needed
+            }
+          }
+        ]
+      }
+    ]
+  },
+  mode: 'production'
+};
