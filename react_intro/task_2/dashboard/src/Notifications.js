c050a55573cb6c73e7d8b9ebe2de484c85674642
Nomad-Rob <rfarley89@gmail.com>
2024-05-01 13:34:35 -0500

    Task 3 complete

--- react_intro/task_2/dashboard/src/Notifications.js
+++ react_intro/task_2/dashboard/src/Notifications.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import './Notifications.css';
+import { getLatestNotification } from './utils';
 
 function Notifications() {
+  const handleClick = () => {
+    console.log('Close button has been clicked');
+  };
+
   return (
-    <div className="Notifications">
+    <div className="Notifications" style={{ position: 'relative', padding: '10px', marginBottom: '20px' }}>
+      <button 
+        style={{ position: 'absolute', top: '10px', right: '10px', cursor: 'pointer' }} 
+        aria-label="Close"
+        onClick={handleClick}>
+        x
+      </button>
       <p>Here is the list of notifications</p>
+      <ul>
+        <li data-priority="default">New course available</li>
+        <li data-priority="urgent">New resume available</li>
+        <li data-priority="urgent" dangerouslySetInnerHTML={{ __html: getLatestNotification() }}></li>
+      </ul>
     </div>
   );
 }
--- react_intro/task_2/dashboard/src/utils.js
+++ react_intro/task_2/dashboard/src/utils.js
@@ -1,10 +1,17 @@
+// Gets the full year
 export function getFullYear() {
   return new Date().getFullYear();
 }
 
+// Gets the footer copy
 export function getFooterCopy(isIndex) {
   if (isIndex) {
     return 'Holberton School';
   }
   return 'Holberton School main dashboard';
+}
+
+// Returns a string for notifications
+export function getLatestNotification() {
+  return '<strong>Urgent requirement</strong> - complete by EOD';
 }
