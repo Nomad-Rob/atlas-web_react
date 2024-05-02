3636c166efe02dd2aea4dffe12e7a38142ba7497
Nomad-Rob <rfarley89@gmail.com>
2024-05-02 13:57:15 -0500

    Task 7 complete

---
+++ react_intro/task_4/dashboard/src/Notifications.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Notifications.css';
+import { getLatestNotification } from './utils';
+
+function Notifications() {
+  const handleClick = () => {
+    console.log('Close button has been clicked');
+  };
+
+  return (
+    <div className="Notifications" style={{ position: 'relative', padding: '10px', marginBottom: '20px' }}>
+      <button 
+        style={{ position: 'absolute', top: '10px', right: '10px', cursor: 'pointer' }} 
+        aria-label="Close"
+        onClick={handleClick}>
+        x
+      </button>
+      <p>Here is the list of notifications</p>
+      <ul>
+        <li data-priority="default">New course available</li>
+        <li data-priority="urgent">New resume available</li>
+        <li data-priority="urgent" dangerouslySetInnerHTML={{ __html: getLatestNotification() }}></li>
+      </ul>
+    </div>
+  );
+}
+
+export default Notifications;
