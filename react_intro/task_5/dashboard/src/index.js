9b14d1960b7ce17a71df315b9aae214da1f1f278
Nomad-Rob <rfarley89@gmail.com>
2024-05-03 10:01:08 -0500

    Task 9 completed. Had some code previously done when stuck on last task

--- react_intro/task_5/dashboard/src/index.js
+++ react_intro/task_5/dashboard/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App/App';
-import { Notifications } from './Notifications/Notifications';
+import Notifications from './Notifications/Notifications';
+
 
 // Root for the main app
 const root = ReactDOM.createRoot(document.getElementById('root'));
