5776e04ccf02fc312e41b0ce921b52f2b697bf7a
Nomad-Rob <rfarley89@gmail.com>
2024-05-08 13:31:18 -0500

    All task complete. Need to fix css still and some small errors

--- React_component/task_4/dashboard/src/Notifications/Notifications.js
+++ React_component/task_4/dashboard/src/Notifications/Notifications.js
@@ -1,66 +1,49 @@
 import React, { Component } from 'react';
-import './Notifications.css';
-import NotificationItem from "./NotificationItem";
-import { getLatestNotification } from "../utils/utils";
 import PropTypes from 'prop-types';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import NotificationItem from './NotificationItem';
 
 class Notifications extends Component {
-  constructor(props) {
-    super(props);
-    this.markAsRead = this.markAsRead.bind(this);
-  }
-
-  markAsRead(id) {
-    console.log(`Notification ${id} has been marked as read`);
+  shouldComponentUpdate(nextProps) {
+    // Only update if new list is longer than the current list
+    return nextProps.listNotifications.length > this.props.listNotifications.length;
   }
 
   render() {
-    const { displayDrawer, listNotifications } = this.props;
     return (
-      <>
-        <div className="menuItem">
-          <p>Your Notifications</p>
-        </div>
-        {displayDrawer && (
-          <div className="Notifications">
-            <div className="Notifications-content">
-              <p>Here is the list of notifications</p>
-              <ul>
-                {listNotifications.map(notification => (
-                  <NotificationItem
-                    key={notification.id}
-                    type={notification.type}
-                    value={notification.value}
-                    html={notification.html}
-                    markAsRead={this.markAsRead}
-                    id={notification.id}
-                  />
-                ))}
-              </ul>
-            </div>
-            <button
-              aria-label="Close"
-              className="close-button"
-              onClick={() => console.log("close button has been clicked")}>
-                <FontAwesomeIcon icon={faTimes} />
-            </button>
-          </div>
-        )}
-      </>
+      <div className='Notifications'>
+        <ul>
+          {this.props.listNotifications.map(notification => (
+            <NotificationItem
+              key={notification.id}
+              id={notification.id}
+              type={notification.type}
+              value={notification.value}
+              html={notification.html}
+              markAsRead={this.props.markAsRead}
+            />
+          ))}
+        </ul>
+      </div>
     );
   }
 }
 
 Notifications.propTypes = {
-  displayDrawer: PropTypes.bool,
-  listNotifications: PropTypes.array.isRequired
+  listNotifications: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    type: PropTypes.string,
+    value: PropTypes.string,
+    html: PropTypes.shape({
+      __html: PropTypes.string
+    }),
+    markAsRead: PropTypes.func
+  })),
+  markAsRead: PropTypes.func
 };
 
 Notifications.defaultProps = {
-  displayDrawer: false,
-  listNotifications: []
+  listNotifications: [],
+  markAsRead: () => {}
 };
 
 export default Notifications;
