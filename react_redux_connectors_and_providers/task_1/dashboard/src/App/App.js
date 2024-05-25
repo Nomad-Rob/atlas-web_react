231aa747939121edf822d235f4b0523329badb0c
Nomad-Rob <rfarley89@gmail.com>
2024-05-25 14:19:43 -0500

    Task 4 completo

--- react_redux_connectors_and_providers/task_1/dashboard/src/App/App.js
+++ react_redux_connectors_and_providers/task_1/dashboard/src/App/App.js
@@ -10,161 +10,146 @@ import BodySectionWithMarginBottom from '../BodySection/BodySectionWithMarginBot
 import BodySection from '../BodySection/BodySection';
 import AppContext from './AppContext';
 import { connect } from 'react-redux';
+import { displayNotificationDrawer, hideNotificationDrawer } from '../actions/uiActionCreators';
 
 const styles = StyleSheet.create({
   body: {
-    textAlign: 'center',
+      textAlign: 'center',
   },
   headerWrapper: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'flex-start',
-    borderBottom: '5px solid #00005C',
-    width: '100%',
+      display: 'flex',
+      justifyContent: 'space-between',
+      alignItems: 'flex-start',
+      borderBottom: '5px solid #00005C',
+      width: '100%',
   },
   headerNotifications: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'flex-end',
-    justifyContent: 'flex-end',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'flex-end',
+      justifyContent: 'flex-end',
   },
   footer: {
-    fontFamily: "'Galano Grotesque Alt', sans-serif",
-    fontStyle: 'italic',
-    fontSize: '1rem',
-    borderTop: '5px solid #00003C',
-    width: '100%',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    position: 'absolute',
-    bottom: '0',
+      fontFamily: "'Galano Grotesque Alt', sans-serif",
+      fontStyle: 'italic',
+      fontSize: '1rem',
+      borderTop: '5px solid #00003C',
+      width: '100%',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      position: 'absolute',
+      bottom: '0',
   }
 });
 
-const listCourses = [
-  { id: 1, name: 'ES6', credit: 60 },
-  { id: 2, name: 'Webpack', credit: 20 },
-  { id: 3, name: 'React', credit: 40 },
-];
-
-const listNotifications = [
-  { id: 1, type: 'default', value: 'New course available' },
-  { id: 2, type: 'urgent', value: 'New resume available' },
-  { id: 3, type: 'urgent', html: { __html: '<strong>Urgent requirement</strong> - complete by EOD' } },
-];
-
 const mapStateToProps = state => {
   return {
-    isLoggedIn: state.ui.isUserLoggedIn,
-    displayDrawer: state.ui.isNotificationDrawerVisible
+      isLoggedIn: state.ui.isUserLoggedIn,
+      displayDrawer: state.ui.isNotificationDrawerVisible
   };
+};
+
+const mapDispatchToProps = {
+  displayNotificationDrawer,
+  hideNotificationDrawer
 };
 
 class App extends Component {
   constructor(props) {
-    super(props);
-    this.state = {
-      displayDrawer: false,
-      user: {
-        email: '',
-        password: '',
-        isLoggedIn: false
-      },
-      listNotifications: [
-        { id: 1, type: 'default', value: 'New course available' },
-        { id: 2, type: 'urgent', value: 'New resume available' },
-        { id: 3, type: 'urgent', html: { __html: '<strong>Urgent requirement</strong> - complete by EOD' } }
-      ]
-    };
+      super(props);
+      this.state = {
+          user: {
+              email: '',
+              password: '',
+              isLoggedIn: false
+          },
+          listNotifications: [
+              { id: 1, type: 'default', value: 'New course available' },
+              { id: 2, type: 'urgent', value: 'New resume available' },
+              { id: 3, type: 'urgent', html: { __html: '<strong>Urgent requirement</strong> - complete by EOD' } }
+          ]
+      };
   }
 
   logIn = (email, password) => {
-    this.setState({
-      user: {
-        email,
-        password,
-        isLoggedIn: true
-      }
-    });
+      this.setState({
+          user: {
+              email,
+              password,
+              isLoggedIn: true
+          }
+      });
   };
 
   logOut = () => {
-    this.setState({
-      user: {
-        email: '',
-        password: '',
-        isLoggedIn: false
-      }
-    });
-  };
-
-  handleDisplayDrawer = () => {
-    this.setState({ displayDrawer: true });
-  };
-
-  handleHideDrawer = () => {
-    this.setState({ displayDrawer: false });
+      this.setState({
+          user: {
+              email,
+              password,
+              isLoggedIn: false
+          }
+      });
   };
 
   markNotificationAsRead = (id) => {
-    this.setState(prevState => ({
-      listNotifications: prevState.listNotifications.filter(notification => notification.id !== id)
-    }));
+      this.setState(prevState => ({
+          listNotifications: prevState.listNotifications.filter(notification => notification.id !== id)
+      }));
   };
 
   componentDidMount() {
-    document.addEventListener('keydown', this.handleKeydown);
+      document.addEventListener('keydown', this.handleKeydown);
   }
 
   componentWillUnmount() {
-    document.removeEventListener('keydown', this.handleKeydown);
+      document.removeEventListener('keydown', this.handleKeydown);
   }
 
   handleKeydown = (event) => {
-    if (event.ctrlKey && event.key === 'h') {
-      event.preventDefault();
-      this.logOut();
-    }
+      if (event.ctrlKey && event.key === 'h') {
+          event.preventDefault();
+          this.logOut();
+      }
   };
 
   render() {
-    const { isLoggedIn, listNotifications } = this.props; // Ensure proper destructuring from props
+      const { isLoggedIn, displayDrawer, listNotifications, displayNotificationDrawer, hideNotificationDrawer } = this.props;
 
-    return (
-      <AppContext.Provider value={{ user: this.state.user, logOut: this.logOut }}>
-        <div className={css(styles.headerWrapper)}>
-          <Header />
-          <div className={css(styles.headerNotifications)}>
-            <Notifications
-              listNotifications={listNotifications}
-              displayDrawer={this.state.displayDrawer}
-              handleDisplayDrawer={this.handleDisplayDrawer}
-              handleHideDrawer={this.handleHideDrawer}
-              markNotificationAsRead={this.markNotificationAsRead}
-            />
-          </div>
-        </div>
-        <div className={css(styles.body)}>
-          {isLoggedIn ? (
-            <BodySectionWithMarginBottom title='Course List'>
-              <CourseList listCourses={listCourses} />
-            </BodySectionWithMarginBottom>
-          ) : (
-            <BodySectionWithMarginBottom title='Log in to continue'>
-              <Login logIn={this.logIn} />
-            </BodySectionWithMarginBottom>
-          )}
-          <div>
-            <BodySection title='News from the School'>
-              <p>This is the latest news from our school community made by Rob!</p>
-            </BodySection>
-          </div>
-        </div>
-        <Footer />
-      </AppContext.Provider>
-    );
+      return (
+          <AppContext.Provider value={{ user: this.state.user, logOut: this.logOut }}>
+              <div className={css(styles.headerWrapper)}>
+                  <Header />
+                  <div className={css(styles.headerNotifications)}>
+                      <Notifications
+                          listNotifications={listNotifications}
+                          displayDrawer={displayDrawer}
+                          handleDisplayDrawer={displayNotificationDrawer}
+                          handleHideDrawer={hideNotificationDrawer}
+                          markNotificationAsRead={this.markNotificationAsRead}
+                      />
+                  </div>
+              </div>
+              <div className={css(styles.body)}>
+                  {isLoggedIn ? (
+                      <BodySectionWithMarginBottom title='Course List'>
+                          <CourseList listCourses={listCourses} />
+                      </BodySectionWithMarginBottom>
+                  ) : (
+                      <BodySectionWithMarginBottom title='Log in to continue'>
+                          <Login logIn={this.logIn} />
+                      </BodySectionWithMarginBottom>
+                  )}
+                  <div>
+                      <BodySection title='News from the School'>
+                          <p>This is the latest news from our school community made by Rob!</p>
+                      </BodySection>
+                  </div>
+              </div>
+              <Footer />
+          </AppContext.Provider>
+      );
   }
 }
 
-export default connect(mapStateToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
