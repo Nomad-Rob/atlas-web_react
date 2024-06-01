23d984d2e88ac277bbf455dabcf79207b505c163
Nomad-Rob <rfarley89@gmail.com>
2024-06-01 13:21:37 -0500

    task 9 complete

--- react_redux_connectors_and_providers/task_2/dashboard/src/Footer/Footer.js
+++ react_redux_connectors_and_providers/task_2/dashboard/src/Footer/Footer.js
@@ -1,25 +1,25 @@
-import React, { useContext } from 'react';
+import React from 'react';
+import { getFooterCopy, getFullYear } from "../utils/utils";
 import './Footer.css';
-import { getFooterCopy, getFullYear } from '../utils/utils';
-import AppContext from '../App/AppContext'; 
+import { connect } from 'react-redux';
 
-function Footer() {
-  // Using useContext to subscribe to the AppContext
-  const { user } = useContext(AppContext);
-
-  return (
-    <footer className="App-footer">
-      <p>
-        Copyright {getFullYear()} - {getFooterCopy(true)}
-      </p>
-      {/* Conditionally rendering the Contact us link based on the user's login status */}
-      {user.isLoggedIn && (
-        <p>
-          <a href="/contact">Contact us</a>
-        </p>
-      )}
-    </footer>
-  );
+function Footer({ user }) { // Destructuring user from props here
+    return (
+        <footer className="App-footer">
+            <p>
+                Copyright {getFullYear()} - {getFooterCopy(true)}
+                {user.isLoggedIn && (
+                    <a href="#contact">Contact us</a>
+                )}
+            </p>
+        </footer>
+    );
 }
 
-export default Footer;
+export const mapStateToProps = (state) => {
+    const uiReducer = state.ui; // Getting uiReducer from state
+    const isLoggedIn = uiReducer.get('isUserLoggedIn', false);
+    return { user: { isLoggedIn } };
+};
+
+export default connect(mapStateToProps)(Footer);
